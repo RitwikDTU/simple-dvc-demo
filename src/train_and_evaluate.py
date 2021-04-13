c41983bc3908852664e4039005c7eb9bcfc4ec1a
Ritwik Shekhar <ritwikdtu@gmail.com>
2021-04-13 16:09:25 +0530

    stage 3 processing

---
+++ src/train_and_evaluate.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 13 15:38:49 2021
+
+@author: ritwi
+"""
+
+# load the train and test files
+# train algorithm
+#save the metrics, parameters
+
+
+import os
+import sys
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error,r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+from get_data import read_params
+import argparse
+import joblib
+import json
+
+
+
+def eval_metrics(actual, pred):
+    rmse=np.sqrt(mean_squared_error(actual,pred))
+    mae=mean_absolute_error(actual,pred)
+    r2=r2_score(actual,pred)
+    return rmse,mae,r2
+
+
+
+def train_and_evaluate(config_path):
+    config=read_params(config_path)
+    test_data_path=config["split_data"]["test_path"]
+    train_data_path=config["split_data"]["train_path"]
+    random_state=config["base"]["random_state"]
+    model_dir=config["model_dir"]
+    
+    
+    alpha=config["estimators"]["ElasticNet"]["params"]["alpha"]
+    l1_ratio=config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
+    
+    target=[config["base"]["target_col"]] #bahar me list use kiye kyuki we want target col as a value and not dataframe
+    
+    train=pd.read_csv(train_data_path,sep=",")
+    test=pd.read_csv(test_data_path,sep=",")
+    
+    
+    train_y=train[target]
+    test_y=test[target]
+    
+    
+    train_x=train.drop(target,axis=1)
+    test_x=test.drop(target,axis=1)
+    
+    lr=ElasticNet(alpha=alpha,
+                  l1_ratio=l1_ratio,
+                  random_state=random_state)
+    lr.fit(train_x,train_y)
+    
+    
+    predicted_qualities=lr.predict(test_x)
+    
+    (rmse,mae,r2)= eval_metrics(test_y,predicted_qualities)
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+if __name__=="__main__":
+    args=argparse.ArgumentParser()
+    args.add_argument("--config",default="params.yaml")
+    parsed_args=args.parse_args()
+    train_and_evaluate(config_path=parsed_args.config)
