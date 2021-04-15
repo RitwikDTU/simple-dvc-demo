2dbd6e35b18c5e3bf2a7fcbc4371e2d5290709c6
Ritwik Shekhar <ritwikdtu@gmail.com>
2021-04-16 02:00:41 +0530

    flask test

--- app.py
+++ app.py
@@ -5,3 +5,31 @@ Created on Thu Apr 15 11:57:27 2021
 @author: ritwi
 """
 
+from flask import Flask, render_template, request, jsonify
+import os
+import yaml
+import joblib
+import numpy as np
+
+
+params_path="param.yaml"
+webapp_root="webapp"
+
+static_dir=os.path.join(webapp_root,"static")
+template_dir=os.path.join(webapp_root,"templates")
+
+app=Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+
+
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method== "POST":
+        pass
+    else:
+        return render_template("index.html")
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
+    
