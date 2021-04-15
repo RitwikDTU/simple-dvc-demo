d94e0cf99daa62f42b67a0310a9732e883f94636
Ritwik Shekhar <ritwikdtu@gmail.com>
2021-04-16 01:43:32 +0530

    webpage added

---
+++ app.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 11:57:27 2021
+
+@author: ritwi
+"""
+
---
+++ prediction_service/prediction.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 16 01:21:03 2021
+
+@author: ritwi
+"""
+
