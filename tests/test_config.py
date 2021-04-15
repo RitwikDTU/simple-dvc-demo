8e718b04dc7239daf4e64ecd714497ffb06ebbe6
Ritwik Shekhar <ritwikdtu@gmail.com>
2021-04-16 00:58:56 +0530

    pytest

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,8 +4,23 @@ Created on Thu Apr 15 22:21:24 2021
 
 @author: ritwi
 """
+import pytest
+class NotInRange(Exception):
+    def __init__(self,message="value not in range"):
+        self.message=message
+        super().__init__(self.message)
+
+
+
 
 def test_generic():
+    a=5
+    with pytest.raises(NotInRange):
+        if a not in range(10,20):
+            raise NotInRange
+            
+            
+def test_something():
     a=1
     b=1
     assert a==b
